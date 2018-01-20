693aa7e30c64fedcfaaf2c02c105b78967da6dea
Michael Gira <contact@michaelgira.me>
2018-01-19 23:50:07 -0600

    Add basic scaffolding for Expo with TS

--- _universe/entry.js
+++
@@ -1,4 +0,0 @@
-import Expo from 'expo';
-import App from '../src/App.js';
-
-Expo.registerRootComponent(App);
---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import App from './App';
+
+import * as renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+	const rendered = renderer.create(<App />).toJSON();
+	expect(rendered).toMatchSnapshot();
+	expect(rendered).toBeTruthy();
+});

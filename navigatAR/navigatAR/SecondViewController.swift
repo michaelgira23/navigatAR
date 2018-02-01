18b57b2db843b876b006ff9b4e02a46173120113
Michael Gira <michaelgira23@gmail.com>
2018-02-01 00:44:09 -0600

    Add basic Swift project

---
+++ navigatAR/navigatAR/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  navigatAR
+//
+//  Created by Michael Gira on 2/1/18.
+//  Copyright © 2018 MICDS Programming. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

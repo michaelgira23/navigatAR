2bc2555313ae38996483e8b478f352b42e5afc66
Michael Gira <michaelgira23@gmail.com>
2018-02-02 22:09:27 -0600

    Convert spaces to tabs :wink:

--- navigatAR/navigatAR/Building.swift
+++ navigatAR/navigatAR/Building.swift
@@ -9,19 +9,19 @@
 import Firebase
 
 struct Building {
-    let id: String
-    let name: String
-    let admins: [String]
-    // let tags: [TagInfo]
-    
-    static func fromPushKey(root: DataSnapshot, key: String) -> Building {
-        let building = root.childSnapshot(forPath: "buildings/\(key)").value as! [String: Any]
-        
-        return Building(
-            id: key,
-            name: building["name"] as! String,
-            admins: Array((building["admins"] as! [String: Bool]).keys)
-            // tags: Array((building["tags"] as! [String: Bool]).keys).map({ TagInfo.fromPushKey(root: rootSnapshot, key: $0) })
-        )
-    }
+	let id: String
+	let name: String
+	let admins: [String]
+	// let tags: [TagInfo]
+
+	static func fromPushKey(root: DataSnapshot, key: String) -> Building {
+		let building = root.childSnapshot(forPath: "buildings/\(key)").value as! [String: Any]
+
+		return Building(
+			id: key,
+			name: building["name"] as! String,
+			admins: Array((building["admins"] as! [String: Bool]).keys)
+			// tags: Array((building["tags"] as! [String: Bool]).keys).map({ TagInfo.fromPushKey(root: rootSnapshot, key: $0) })
+		)
+	}
 }
--- navigatAR/navigatAR/Node.swift
+++ navigatAR/navigatAR/Node.swift
@@ -9,34 +9,34 @@
 import Firebase
 
 enum NodeType {
-    // Generic stuff
-    case pointOfInterest
-    
-    // More specific nodes
-    case pathway
-    case bathroom
-    case printer
-    case fountain
-    case room
-    case sportsVenue
-    
-    // TODO: add more types if necessary
+	// Generic stuff
+	case pointOfInterest
+
+	// More specific nodes
+	case pathway
+	case bathroom
+	case printer
+	case fountain
+	case room
+	case sportsVenue
+
+	// TODO: add more types if necessary
 }
 
 struct Node {
-    let building: Building
-    let name: String
-    let admins: [String]
-    // let tags: [String: Any] TODO
-    
-    static func fromPushKey(root: DataSnapshot, key: String) -> Node {
-        let node = root.childSnapshot(forPath: "nodes/\(key)").value as! [String: Any]
-        
-        return Node(
-            building: Building.fromPushKey(root: root, key: node["building"] as! String),
-            name: node["name"] as! String,
-            admins: Array((node["admins"] as! [String: Bool]).keys)
-            // tags: whatever
-        )
-    }
+	let building: Building
+	let name: String
+	let admins: [String]
+	// let tags: [String: Any] TODO
+
+	static func fromPushKey(root: DataSnapshot, key: String) -> Node {
+		let node = root.childSnapshot(forPath: "nodes/\(key)").value as! [String: Any]
+
+		return Node(
+			building: Building.fromPushKey(root: root, key: node["building"] as! String),
+			name: node["name"] as! String,
+			admins: Array((node["admins"] as! [String: Bool]).keys)
+			// tags: whatever
+		)
+	}
 }

60588ad2421ad920232da0b43e4b0f5d85febb1f
Nick Clifford <nick@nickclifford.me>
2018-02-03 15:43:08 -0600

    add location to Node

--- navigatAR/navigatAR/Node.swift
+++ navigatAR/navigatAR/Node.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 MICDS Programming. All rights reserved.
 //
 
+import CoreLocation
 import Firebase
 
 enum NodeType: String {
@@ -29,18 +30,26 @@ struct Node: FirebaseModel {
     let building: FirebasePushKey
     let name: String
 	let type: NodeType
-	// let position: CLLocation TODO
+	let position: CLLocation
     // let tags: [String: Any] TODO
     
     static func fromPushKey(root: DataSnapshot, key: FirebasePushKey) -> Node {
         let node = root.childSnapshot(forPath: "nodes/\(key)").value as! [String: Any]
+		
+		let position = node["position"] as! [String: Double]
         
         return Node(
 			id: key,
             building: node["building"] as! FirebasePushKey,
             name: node["name"] as! String,
-			type: NodeType(rawValue: node["type"] as! String)!
-			// TODO: the rest of these
+			type: NodeType(rawValue: node["type"] as! String)!,
+			position: CLLocation(
+				coordinate: CLLocationCoordinate2DMake(position["latitude"]!, position["longitude"]!),
+				altitude: position["altitude"]!,
+				horizontalAccuracy: kCLLocationAccuracyBestForNavigation, // TODO: maybe change this to kCLLocationAccuracyBest?
+				verticalAccuracy: kCLLocationAccuracyBestForNavigation,
+				timestamp: Date()
+			)
         )
     }
 }

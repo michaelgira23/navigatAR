38573c45f431d387a15a2581cda4f1b8adfe6591
Nick Clifford <nick@nickclifford.me>
2018-02-15 16:59:03 -0600

    add Location#distanceTo(_:)

--- navigatAR/navigatAR/Location.swift
+++ navigatAR/navigatAR/Location.swift
@@ -26,4 +26,16 @@ struct Location: Codable {
 		verticalAccuracy = ial.location!.verticalAccuracy
 		horizontalAccuracy = ial.location!.horizontalAccuracy
 	}
+	
+	func distanceTo(_ other: Location) -> Double {
+		// https://en.wikipedia.org/wiki/Geographic_coordinate_system#Expressing_latitude_and_longitude_as_linear_units
+		let metersPerLatitude = 111132.92 - (559.82 * cos(2 * other.latitude)) + (1.175 * cos(4 * other.latitude)) - (0.0023 * cos(6 * other.latitude))
+		let metersPerLongitude = (111412.84 * cos(other.longitude)) - (93.5 * cos(3 * other.longitude)) + (0.118 * cos(5 * other.longitude))
+		
+		let deltaLatitude = fabs(latitude - other.latitude)
+		let deltaLongitude = fabs(longitude - other.longitude)
+		let deltaAltitude = fabs(altitude - other.altitude)
+		
+		return sqrt(pow(deltaLatitude * metersPerLatitude, 2) + pow(deltaLongitude * metersPerLongitude, 2) + pow(deltaAltitude, 2))
+	}
 }

042c31b9885825b9e819ec4082899714043a6180
Nick Clifford <nick@nickclifford.me>
2018-02-15 17:25:42 -0600

    change distanceTo to actually work I think, separate out meters per degree

--- navigatAR/navigatAR/Location.swift
+++ navigatAR/navigatAR/Location.swift
@@ -9,8 +9,23 @@
 import CoreLocation
 import IndoorAtlas
 
-// NOTE: This is used instead of just CLLocation because of Codable conformance.
+// NOTE: This is used instead of just IALocation because of Codable conformance.
 struct Location: Codable {
+	// https://en.wikipedia.org/wiki/Geographic_coordinate_system#Expressing_latitude_and_longitude_as_linear_units
+	static func metersPerLatitude(degrees: Double) -> Double {
+		let radians = degreesToRadians(degrees)
+		
+		// Swift compiler is having difficulty parsing these all inlined for whatever reason
+		let val1 = 559.82 * cos(2 * radians)
+		let val2 = 1.175 * cos(4 * radians)
+		let val3 = 0.0023 * cos(6 * radians)
+		return 111132.92 - val1 + val2 - val3
+	}
+	static func metersPerLongitude(degrees: Double) -> Double {
+		let radians = degreesToRadians(degrees)
+		return (111412.84 * cos(radians)) - (93.5 * cos(3 * radians)) + (0.118 * cos(5 * radians))
+	}
+	
 	let latitude: CLLocationDegrees
 	let longitude: CLLocationDegrees
 	let altitude: CLLocationDistance
@@ -28,14 +43,10 @@ struct Location: Codable {
 	}
 	
 	func distanceTo(_ other: Location) -> Double {
-		// https://en.wikipedia.org/wiki/Geographic_coordinate_system#Expressing_latitude_and_longitude_as_linear_units
-		let metersPerLatitude = 111132.92 - (559.82 * cos(2 * other.latitude)) + (1.175 * cos(4 * other.latitude)) - (0.0023 * cos(6 * other.latitude))
-		let metersPerLongitude = (111412.84 * cos(other.longitude)) - (93.5 * cos(3 * other.longitude)) + (0.118 * cos(5 * other.longitude))
-		
 		let deltaLatitude = fabs(latitude - other.latitude)
 		let deltaLongitude = fabs(longitude - other.longitude)
 		let deltaAltitude = fabs(altitude - other.altitude)
 		
-		return sqrt(pow(deltaLatitude * metersPerLatitude, 2) + pow(deltaLongitude * metersPerLongitude, 2) + pow(deltaAltitude, 2))
+		return sqrt(pow(Location.metersPerLatitude(degrees: deltaLatitude), 2) + pow(Location.metersPerLongitude(degrees: deltaLongitude), 2) + pow(deltaAltitude, 2))
 	}
 }

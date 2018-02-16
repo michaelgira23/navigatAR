855b089438a4535163637687c71519e150064df9
stlgolfer <amigala@micds.org>
2018-02-15 23:51:44 -0600

    Fixed some indentation

--- navigatAR/navigatAR/NewBuildingViewController.swift
+++ navigatAR/navigatAR/NewBuildingViewController.swift
@@ -13,59 +13,59 @@ import CodableFirebase
 import Firebase
 
 class NewBuildingViewController: FormViewController {
-    
-    var regions: [IARegion] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        form +++ Section("Building Information")
-            <<< TextRow("buildingname") { row in
-                row.title = "Name"
-                row.placeholder = "Building Name"
-            }
-        
-            +++ MultivaluedSection(multivaluedOptions: .Insert, header: "Add Floors") { sec in
-                sec.addButtonProvider = { _ in return ButtonRow {
-                    $0.title = "Add Floor"
-                    
-                    }.cellUpdate { cell, row in
-                        cell.textLabel?.textAlignment = .left
-                    }
-                }
-            
-                sec.multivaluedRowToInsertAt = { index in return LabelRow { row in
-                        guard let region = IALocationManager.sharedInstance().location?.region else {
-                            print("oh bother") // TODO: actual error handling
-                            return
-                        }
-                    
-                        row.title = region.name
-                        self.regions.append(region)
-                    }
-                }
-                
-                sec.showInsertIconInAddButton = true
-        }
-            +++ Section()
-            <<< ButtonRow() { row in
-                row.title = "Create"
-                row.onCellSelection { cell, row in
-                    guard let buildingName = self.form.values()["buildingname"] as? String else { return }
-                    
-                    let building = Building(
-                        name: buildingName,
-                        indoorAtlasFloors: self.regions.reduce(into: [:], { (result, region) in result[region.identifier] = region.name })
-                    )
-                    
-                    let ref = Database.database().reference()
-                    ref.child("buildings").childByAutoId().setValue(try! FirebaseEncoder().encode(building));
-                    
-                    _ = self.navigationController?.popViewController(animated: true)
-                }
-        }
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        // view did disappear; we can do cleanup
-    }
+	
+	var regions: [IARegion] = []
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		form +++ Section("Building Information")
+			<<< TextRow("buildingname") { row in
+				row.title = "Name"
+				row.placeholder = "Building Name"
+			}
+			
+			+++ MultivaluedSection(multivaluedOptions: .Insert, header: "Add Floors") { sec in
+				sec.addButtonProvider = { _ in return ButtonRow {
+					$0.title = "Add Floor"
+					
+					}.cellUpdate { cell, row in
+						cell.textLabel?.textAlignment = .left
+					}
+				}
+				
+				sec.multivaluedRowToInsertAt = { index in return LabelRow { row in
+					guard let region = IALocationManager.sharedInstance().location?.region else {
+						print("oh bother") // TODO: actual error handling
+						return
+					}
+					
+					row.title = region.name
+					self.regions.append(region)
+					}
+				}
+				
+				sec.showInsertIconInAddButton = true
+			}
+			+++ Section()
+			<<< ButtonRow() { row in
+				row.title = "Create"
+				row.onCellSelection { cell, row in
+					guard let buildingName = self.form.values()["buildingname"] as? String else { return }
+					
+					let building = Building(
+						name: buildingName,
+						indoorAtlasFloors: self.regions.reduce(into: [:], { (result, region) in result[region.identifier] = region.name })
+					)
+					
+					let ref = Database.database().reference()
+					ref.child("buildings").childByAutoId().setValue(try! FirebaseEncoder().encode(building));
+					
+					_ = self.navigationController?.popViewController(animated: true)
+				}
+		}
+	}
+	
+	override func viewDidDisappear(_ animated: Bool) {
+		// view did disappear; we can do cleanup
+	}
 }

31d2b3e169fe661a95a15837d5b6d57234e99602
Nick Clifford <nick@nickclifford.me>
2018-02-16 21:14:42 -0600

    add login + register view boilerplate

---
+++ navigatAR/navigatAR/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  navigatAR
+//
+//  Created by Nick Clifford on 2/16/18.
+//  Copyright © 2018 MICDS Programming. All rights reserved.
+//
+
+import Eureka
+
+class LoginViewController: FormViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// form building
+	}
+}
---
+++ navigatAR/navigatAR/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  navigatAR
+//
+//  Created by Nick Clifford on 2/16/18.
+//  Copyright © 2018 MICDS Programming. All rights reserved.
+//
+
+import Eureka
+
+class RegisterViewController: FormViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// form building
+	}
+}
+

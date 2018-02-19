2b8491d64919a33b7166bcae0298ba796e779775
stlgolfer <amigala@micds.org>
2018-02-18 22:43:57 -0500

    Table can now load from db

--- navigatAR/navigatAR/SelectNodesViewController.swift
+++ navigatAR/navigatAR/SelectNodesViewController.swift
@@ -14,24 +14,45 @@ class SelectNodesViewController: UIViewController, UITableViewDataSource, UITabl
 	
 	@IBOutlet weak var nodesTable: UITableView!
 	
-	var availableNodes: [String] = ["dummyData"]
+	var availableNodes: [Node] = []
+	var selectedNodes: [Node] = []
 
 	override func viewDidLoad() {
         super.viewDidLoad()
-		self.nodesTable.dataSource = self
-		self.nodesTable.delegate = self
+		self.getNodes()
 		self.nodesTable.reloadData()
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = self.nodesTable.dequeueReusableCell(withIdentifier: "availableNodes", for: indexPath) as UITableViewCell
 		
-		cell.textLabel?.text = self.availableNodes[indexPath.row]
+		cell.textLabel?.text = self.availableNodes[indexPath.row].name
 		
 		return cell
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return self.availableNodes.count
+	}
+	
+	func getNodes() {
+		let ref = Database.database().reference()
+		ref.child("nodes").observe(.value, with: { (snapshot) in
+			guard let value = snapshot.value else { return }
+			
+			do {
+				let firebaseNodes = Array(try FirebaseDecoder().decode([FirebasePushKey: Node].self, from: value))
+				
+				for node in firebaseNodes {
+					self.availableNodes.append(node.value)
+				}
+			}
+			catch let error {
+				print(error)
+			}
+			
+			self.nodesTable.dataSource = self
+			self.nodesTable.delegate = self
+		})
 	}
 }
